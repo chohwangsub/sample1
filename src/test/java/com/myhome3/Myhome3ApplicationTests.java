e640ceefd7696527d44656aba0b0d4ae4a25a5a0
chohwangsub <33279744+chohwangsub@users.noreply.github.com>
2025-04-20 21:30:13 +0900

    Add files via upload

---
+++ src/test/java/com/myhome3/Myhome3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhome3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myhome3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
